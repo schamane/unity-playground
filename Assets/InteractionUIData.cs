b4686667c13370f99736296285824e42bf485273
Nazar Kulyk <schamane@myeburg.net>
2018-08-29 23:58:57 +0200

    some interaction with gui

---
+++ Assets/InteractionUIData.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractionUIData : MonoBehaviour {
+
+	public string DisplayName;
+
+	void Start() {}
+}
